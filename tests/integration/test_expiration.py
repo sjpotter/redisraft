b20d45595bc13c00e1c61a8dcb1f742a24bcbe3e
Shaya Potter <shaya@redislabs.com>
2023-04-27 14:13:18 +0300

    nits

--- tests/integration/test_expiration.py
+++ tests/integration/test_expiration.py
@@ -6,18 +6,18 @@ def test_expiration_log_entry(cluster):
     cluster.execute("expire", "x", 1000)
     cluster.execute("expire", "y", 1000)
 
+    # first test with a ttl that won't match
     cluster.execute("raft.debug", "expire", "x", "y", 1)
-
     cluster.wait_for_unanimity()
 
-    for i in range(1,4):
-        assert cluster.node(i).execute("raft.debug", "exec", "get", "x") == b'1'
-        assert cluster.node(i).execute("raft.debug", "exec", "get", "y") == b'2'
+    for _, n in cluster.nodes.items():
+        assert n.execute("raft.debug", "exec", "get", "x") == b'1'
+        assert n.execute("raft.debug", "exec", "get", "y") == b'2'
 
+    # second test with a ttl that will match
     cluster.execute("raft.debug", "expire", "x", "y", 0)
-
     cluster.wait_for_unanimity()
 
-    for i in range(1,4):
-        assert cluster.node(i).execute("raft.debug", "exec", "get", "x") == None
-        assert cluster.node(i).execute("raft.debug", "exec", "get", "y") == None
+    for _, n in cluster.nodes.items():
+        assert n.execute("raft.debug", "exec", "get", "x") is None
+        assert n.execute("raft.debug", "exec", "get", "y") is None
